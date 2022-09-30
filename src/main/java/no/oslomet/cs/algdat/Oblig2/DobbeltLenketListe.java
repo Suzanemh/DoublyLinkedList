bffc33734331f4131c65129ef30ba3a745e1dcfe
Suzanemh <91255185+Suzanemh@users.noreply.github.com>
2022-09-30 11:06:03 +0200

    Oppgave 3a: Oppgave 3B Kildekode hentet fra 1.2.3.a. FratilKontroll metode

--- src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
@@ -70,7 +70,23 @@ public class DobbeltLenketListe<T> implements Liste<T> {
         }
     }
 
+    //Oppgave 3B Kildekode hentet fra 1.2.3.a
+    public static void fratilKontroll(int antall, int fra, int til)
+    {
+        if (fra < 0)                                  // fra er negativ
+            throw new IndexOutOfBoundsException("fra(" + fra + ") er negativ!");
+
+        if (til > antall)                          // til er utenfor tabellen
+            throw new IndexOutOfBoundsException("til(" + til + ") > antall(" + antall + ")");
+
+        if (fra > til)                                // fra er større enn til
+            throw new IllegalArgumentException
+                    ("fra(" + fra + ") > til(" + til + ") - illegalt intervall!");
+    }
+
+
     public Liste<T> subliste(int fra, int til) {
+
         throw new UnsupportedOperationException();
     }
 
